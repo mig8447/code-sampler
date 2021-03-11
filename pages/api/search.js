ca0094a33166d9d0298a64c07a443487a6defd86
fabianrmz <a01630024@itesm.mx>
2021-03-11 13:21:34 -0600

    search request changed, apply trim to param

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,11 +1,11 @@
 import { posts } from '../../cache/data';
 
 export default (req, res) =>{
-    
-    const results = req.query.q ? posts.filter((post) => 
-        post.title.toLowerCase().includes(req.query.q) ||  
-        post.description.toLowerCase().includes(req.query.q)  || 
-        post.tags.toLocaleString().toLowerCase().split(",").includes(req.query.q)  )  : []
+    const param = req.query.q.toLowerCase().trim();
+    const results = param ? posts.filter((post) => 
+        post.title.toLowerCase().includes(param) ||  
+        post.description.toLowerCase().includes(param)  || 
+        post.tags.toLocaleString().toLowerCase().split(",").includes(param)  )  : []
     
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
