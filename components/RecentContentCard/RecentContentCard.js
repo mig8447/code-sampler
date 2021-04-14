3a5cb22459fd1a2394acdb73b63b18566c8f08af
Erick <erick_tec@outlook.com>
2021-04-14 17:22:17 -0500

    feat: save into localstorage favorite samples

---
+++ components/RecentContentCard/RecentContentCard.js
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import { Card, Row, Col } from "react-bootstrap";
+import Tags from "../UI/Tags";
+import PropTypes from 'prop-types';
+import { useState, useEffect } from "react";
+
+
+const RecentContentCard = ({ title, description, tags, filename, favorite, onClickFavorite }) => {
+
+    const icon = favorite  ? "fa fa-bookmark fa-sm fa-2x text-warning" : "fa fa-bookmark-o fa-sm fa-2x"
+    const action = favorite ? "delete" : "add";
+
+    return (
+
+        <Card style={{ backgroundColor: "#dae2ef", flexBasis: "30%" }} >
+            <Card.Body>
+                <Card.Title className={"text-capitalize"}>
+                    <Row>
+                        <Col>
+                            <Link href={`/${filename}`}>
+                                {title}
+                            </Link>
+                        </Col>
+                        <Col className={["d-flex", "justify-content-end"].join(" ")}>
+                            <button onClick={() => onClickFavorite(action, filename)} style={{backgroundColor:"transparent"}} className="border-0" >
+                                <span className={icon} aria-hidden="true"></span>
+                            </button>
+                            
+                        </Col>
+                    </Row>
+                </Card.Title>
+                <Tags tags={tags} />
+                <Card.Text>
+                    {description}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+
+    )
+}
+
+RecentContentCard.propTypes = {
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+    filename: PropTypes.string.isRequired
+};
+
+export default RecentContentCard;
+
+
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,44 @@
-
 import fs from "fs";
 import path from 'path';
 import Link from "next/link";
 import matter from 'gray-matter';
 import Navbar from '../components/Navbar/Navbar';
 import Searchbar from '../components/Searchbar/Searchbar';
-import RecentContentCard from '../components/RecentContent/RecentContentCard/RecentContentCard';
+import RecentContentCard from '../components/RecentContentCard/RecentContentCard';
 import ContentCards from '../components/ContentCards/ContentCards';
 import Head from 'next/head';
 import { Container } from "react-bootstrap";
 import TopCategoryCard from "../components/TopCategoryCard/TopCategoryCard";
+import { useEffect, useState } from "react";
 
 const Index = ({ slugs, recentContent }) => {
+
+  const [favorites, setFavorites] = useState();
+
+  useEffect(() => {
+    setFavorites(JSON.parse(localStorage.getItem("favorites")))
+  }, [])
+
+  const deleteKeyFromObject = (key) => {
+    const { [key]: tmp, ...rest } = favorites
+    console.log(rest)
+    setFavorites(rest);
+    localStorage.setItem("favorites", JSON.stringify(rest));
+  }
+
+  const onClickFavorite = (action, filename) => {
+    console.log("actions", action)
+    if (action === "delete") {
+      deleteKeyFromObject(filename);
+    } else if (action === "add") {
+      let newFavorites = { ...favorites }
+      newFavorites[filename] = true;
+      console.log(newFavorites)
+      setFavorites(newFavorites)
+      localStorage.setItem("favorites", JSON.stringify(newFavorites));
+    }
+  }
+
 
   return (
     <div>
@@ -24,19 +51,21 @@ const Index = ({ slugs, recentContent }) => {
       <Container fluid className="mt-3 mb-3">
         <h2>Recent content</h2>
         <ContentCards className="{mt-5}">
-          {recentContent.map(metaData =>(
-            <RecentContentCard 
-              key={metaData.filename} 
-              filename={metaData.filename} 
-              title={metaData.title} 
-              description={metaData.description}  
-              tags={metaData.tags} 
+          {recentContent.map(metaData => (
+            <RecentContentCard
+              key={metaData.filename}
+              filename={metaData.filename}
+              title={metaData.title}
+              description={metaData.description}
+              tags={metaData.tags}
+              onClickFavorite={onClickFavorite}
+              favorite={favorites && favorites[metaData.filename]}
             />
           ))}
-          
+
         </ContentCards>
       </Container>
-      
+
       <Container fluid>
         <h2>Top Categories</h2>
         <ContentCards >
@@ -67,25 +96,24 @@ const Index = ({ slugs, recentContent }) => {
 
 export const getStaticProps = async () => {
   const files = fs.readdirSync("posts");
-
   const slugs = files.map(filename => {
     const markdownWithMetadata = fs.readFileSync(path.join('posts', filename)).toString();
-    const parsedMarkwdown = matter(markdownWithMetadata);
+    const parsedMarkdown = matter(markdownWithMetadata);
     const metaData = {
       filename: filename.replace(".md", ""),
-      title: parsedMarkwdown.data.title,
-      created: parsedMarkwdown.data.created.toString(),
-      lastUpdated: parsedMarkwdown.data.lastUpdated.toString(),
-      tags: parsedMarkwdown.data.tags,
-      description: parsedMarkwdown.data.description,
-      published: parsedMarkwdown.data.published
+      title: parsedMarkdown.data.title,
+      created: parsedMarkdown.data.created.toString(),
+      lastUpdated: parsedMarkdown.data.lastUpdated.toString(),
+      tags: parsedMarkdown.data.tags,
+      description: parsedMarkdown.data.description,
+      published: parsedMarkdown.data.published
     }
 
     return metaData
 
   });
 
-  const recentContent = slugs.sort((a, b) =>{
+  const recentContent = slugs.sort((a, b) => {
     const dateA = new Date(a.created);
     const dateB = new Date(b.created);
     return dateB - dateA;
@@ -94,7 +122,7 @@ export const getStaticProps = async () => {
   return {
     props: {
       slugs,
-      recentContent: recentContent.slice(0,3)
+      recentContent: recentContent.slice(0, 3)
     }
   };
 };
