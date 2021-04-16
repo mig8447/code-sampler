76371474a91e032a0d2e7ac3d53aef9ee61a1375
Erick <erick_tec@outlook.com>
2021-04-16 17:55:53 -0500

    feat: search bar's drop down menu with responsive features

--- components/Searchbar/Searchbar.js
+++ components/Searchbar/Searchbar.js
@@ -1,32 +1,53 @@
 import { Navbar, Form, FormControl, Button, InputGroup, Row, Col, Container } from 'react-bootstrap';
+import { useState } from 'react';
 import Style from '../../styles/Searchbar.module.css';
 
-const SearchBarComponent = ({ query, setQuery }) => {
+const SearchBarComponent = () => {
 
-    const basePath = process.env.NEXT_BASE_PATH || "";
+    const [query, setQuery] = useState("");
+
+    const onChangeQuery = (event) => {
+        setQuery(event.target.value)
+    }
 
     return (
-            <Navbar className={Style.searchBarColor} >
-                <Container className={"d-flex justify-content-center"} >
-                    <InputGroup className={"w-50 p-2 " +  Style.searchBar }>
-                        <FormControl
-                            className={"border-0"}
-                            placeholder="How to.."
-                            aria-label="Search"
-                            value={query}
-                            onChange={setQuery}
-                            />
-                        <InputGroup.Append>
-                            <InputGroup.Text className={"bg-white border-0"}>
+        <Navbar className={Style.searchBarColor} >
+            <Container fluid="md" className={"d-flex justify-content-center"} >
+                
+                <InputGroup className={["w-50", "p-2", Style.searchBar].join(" ")}>
+                    <FormControl
+                        className={["border-0", query ? Style.borderRadius: ""].join(" ")}
+                        placeholder="How to.."
+                        aria-label="Search"
+                        type="text"
+                        value={query}
+                        onChange={onChangeQuery}
+                    />
+                        
+                    <InputGroup.Append>
+                        <InputGroup.Text className={"bg-white border-0"}>
                             <span className="fa fa-search fa-sm" aria-hidden="true"></span>
-                            </InputGroup.Text>
-                        </InputGroup.Append>
+                        </InputGroup.Text>
+                    </InputGroup.Append>
 
-                    </InputGroup>
-                    <span className="fa fa-filter fa-lg text-light" aria-hidden="true"></span>
-                </Container>
+                    <div style={{
+                        display: query ? 'block' : 'none',
+                        borderBottomLeftRadius: "0.25rem",
+                        borderBottomRightRadius: "0.25rem"
+                    }}
+                        className={["bg-white", Style.dropDown].join(" ")}>
+                        <ul>
+                            <li>
+                                test
+                            </li>
+                        </ul>
+                    </div>
 
-            </Navbar>
+                </InputGroup>
+                <span className="fa fa-filter fa-lg text-light" aria-hidden="true"></span>
+            </Container>
+
+        </Navbar>
     )
 }
 
