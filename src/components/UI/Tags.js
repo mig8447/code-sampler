2ac315682e1d2cd08188db3aed0393ab7b57a9f3
Gussiny <a01635151@itesm.mx>
2021-05-31 16:19:15 -0500

    feat: change tags design

--- src/components/UI/Tags.js
+++ src/components/UI/Tags.js
@@ -3,7 +3,7 @@ import classes from "../../styles/Tags.module.css";
 const Tags = ({tags}) => (
     <div className={["d-flex", "flex-row", "flex-wrap", "mb-2", classes.tags].join(" ")} >
         {tags.map(tag => (
-            <p className={["text-capitalize", "mb-1", "mr-1", "text-truncate", classes.tag].join(" ")} key={tag}>{tag}</p>
+            <p className={["text-capitalize", "mb-1 mr-1", "text-truncate", "px-2", classes.tag].join(" ")} key={tag}>{tag}</p>
         )
         )}
     </div>
