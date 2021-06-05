1afc3903a432c1fde80bbc47b5acaa1cfe7005fb
fabianrmz <a01630024@itesm.mx>
2021-06-04 21:49:04 -0500

    feat: document fuse implementation script

--- src/components/useFuse/useFuse.js
+++ src/components/useFuse/useFuse.js
@@ -1,66 +1,77 @@
-// features/useFuse.js
-import Fuse from 'fuse.js';
 import { useMemo, useState } from 'react';
+import Fuse from 'fuse.js';
 
-
-export const useFuse = (list, options, keyword="",filtersToApply={}) => {
+/**
+ * An unified Fuse implementation for optimization and reuse purposes method.
+ * @param {Array<Object>} list - The index to create fuse search engine. 
+ * @param {{limit: number, matchAllOnEmptyQuery: boolean}} options - rest of keys are part of fuse documentation.
+ * @param {string} keyword  - Optional parameter if you want to initialize useFuse with a query.
+ * @param {{key: boolean}} filtersToApply Object with the key value as the filter's name.
+ * @returns {{results:Array<Object>, onSearch: Function, query: string, setQuery: Function, setFilters: Function, onSelectFilter: Function, filtersSelected:Array<string>}}
+ */
+export const useFuse = ( list, options, keyword="", filtersToApply={} ) => {
     // defining our query state in there directly
-    const [query, setQuery] = useState(keyword);
-    const [filters, setFilters] = useState(filtersToApply);
-    let filtersSelected = Object.keys(filters);
+    const [ query, setQuery ] = useState( keyword );
+    const [ filters, setFilters ] = useState( filtersToApply );
+    let filtersSelected = Object.keys( filters );
     // removing custom options from Fuse options object
-    // NOTE: `limit` is actually a `fuse.search` option, but we merge all options for convenience
+    // "limit" is a fuse.search option
     const { limit, matchAllOnEmptyQuery, ...fuseOptions } = options;
 
-    // let's memoize the fuse instance for performances
+    /**
+     * @const fuse - fuse object to pass the query and perform the search.
+     */
     const fuse = useMemo(
-        () => new Fuse(list, fuseOptions),
-        [list, fuseOptions]
+        () => new Fuse( list, fuseOptions ),
+        [ list, fuseOptions ]
     );
 
-    // memoize results whenever the query or options change
+
     const results = useMemo(
-        // if query is empty and `matchAllOnEmptyQuery` is `true` then return all list
-        // NOTE: we remap the results to match the return structure of `fuse.search()`
+        // If query is empty and `matchAllOnEmptyQuery` is `true` then return all list
+        // Remap the results to match the return structure of `fuse.search()`
         () => {
             if (!query && matchAllOnEmptyQuery) {
-                return fuse.getIndex().docs.slice(0, limit).map((item, refIndex) => ({ item, refIndex }))
-            } else if (query && filtersSelected.length === 0) {
-                return fuse.search(query);
-            } else if (!query && filtersSelected.length > 0) {
-                return fuse.search({ $and: [{ tags: `'${filtersSelected.join(" '")}` }] });
+                return fuse.getIndex().docs.slice( 0, limit ).map(( item, refIndex ) => ({ item, refIndex }))
+            } else if ( query && filtersSelected.length === 0 ) {
+                return fuse.search( query );
+            } else if ( !query && filtersSelected.length > 0 ) {
+                return fuse.search({ $and: [{ tags: `'${ filtersSelected.join(" '") }` }] });
             }
             else {
-                return fuse.search({ $and: [{ tags: `'${filtersSelected.join(" '")}` }], query })
+                return fuse.search({ $and: [{ tags: `'${ filtersSelected.join(" '") }` }], query })
             }
         },
-        [fuse, limit, matchAllOnEmptyQuery, query, filtersSelected]
+        [ fuse, limit, matchAllOnEmptyQuery, query, filtersSelected ]
     );
 
 
-    const onSelectFilter = (selected, label) => {
-        if (selected) {
-            let { [label]: remove, ...newFilter } = filters;
+    /**
+     * @param {boolean} selected
+     * @param {string} label
+     * filters are an object to prevent a loop every time the user change a filter.
+     */
+    const onSelectFilter = ( selected, label ) => {
+        if ( selected ) {
+            let { [ label ]: remove, ...newFilter } = filters;
 
-            setFilters(newFilter);
-            filtersSelected = Object.keys(newFilter);
+            setFilters( newFilter );
+            filtersSelected = Object.keys( newFilter );
         }
         else {
             let newFilter = {
                 ...filters,
                 [label]: true
             };
-            setFilters(newFilter);
-            filtersSelected = Object.keys(newFilter);
-        }
-    }
-
-    // pass a handling helper to speed up implementation
-    const onSearch =((e) => setQuery(e.target.value));
+            setFilters( newFilter );
+            filtersSelected = Object.keys( newFilter );
+        };
+    };
 
 
+    const onSearch =( (e) => setQuery( e.target.value ) );
 
-    // still returning `setQuery` for custom handler implementations
+
     return {
         results,
         onSearch,
